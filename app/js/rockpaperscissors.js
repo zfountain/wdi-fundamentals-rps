ab37391c40e08500f1e74a41ebb6c0b6c9a557fc
Zachariah Fountain <zachariahfountain@Zachariahs-MacBook-Pro.local>
2015-04-23 11:54:22 -0500

    Removed extra return winner line.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,7 +40,21 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if ((playerMove === 'rock') && (computerMove === 'scissors')) {
+    winner = 'player';
+} else if ((playerMove = 'scissors') && (computerMove === 'paper')) {
+    winner = 'player';
+} else if ((playerMove === 'paper') && (computerMove === 'rock')) {
+    winner = 'player';
+} else if ((computerMove === 'rock') && (playerMove === 'scissors')) {
+    winner = 'computer';
+} else if ((computerMove = 'scissors') && (playerMove === 'paper')) {
+    winner = 'computer';
+} else if ((computerMove === 'paper') && (playerMove === 'rock')) {
+    winner = 'computer';
+} else {
+    winner = 'tie';
+}
     return winner;
 }
 
